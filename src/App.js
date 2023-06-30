e3e2aa449bd56ed29da7bd8076f9d16214e7ccf4
Anatolii Kravchenko <tol890@ukr.net>
2023-06-30 13:34:20 +0300

    [003] - Add the handling events functions in UserInput component

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
       <Header />
 
       <UserInput />
-
+    
       <ResultsTable />
     </div>
   );
--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,38 +1,75 @@
 const UserInput = () => {
-    return (
-        <form className="form">
-            <div className="input-group">
-                <p>
-                    <label htmlFor="current-savings">Current Savings ($)</label>
-                    <input type="number" id="current-savings" />
-                </p>
-                <p>
-                    <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-                    <input type="number" id="yearly-contribution" />
-                </p>
-            </div>
-            <div className="input-group">
-                <p>
-                    <label htmlFor="expected-return">
-                        Expected Interest (%, per year)
-                    </label>
-                    <input type="number" id="expected-return" />
-                </p>
-                <p>
-                    <label htmlFor="duration">Investment Duration (years)</label>
-                    <input type="number" id="duration" />
-                </p>
-            </div>
-            <p className="actions">
-                <button type="reset" className="buttonAlt">
-                    Reset
-                </button>
-                <button type="submit" className="button">
-                    Calculate
-                </button>
-            </p>
-        </form>
-    );
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // ...
+    console.log("SUBMIT");
+  };
+
+  const resetHandler = () => {
+    // ...
+    console.log("RESET");
+  };
+
+  const inputChangeHandler = (input, value) => {
+    console.log(input, value);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className="form">
+      <div className="input-group">
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            onChange={() =>
+              inputChangeHandler("current-savings", window.event.target.value)
+            }
+            type="number"
+            id="current-savings"
+          />
+        </p>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input
+            onChange={() =>
+              inputChangeHandler("yearly-contribution", window.event.target.value)
+            }
+            type="number"
+            id="yearly-contribution"
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input
+            onChange={() =>
+              inputChangeHandler("expected-return", window.event.target.value)
+            }
+            type="number"
+            id="expected-return"
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input onChange={() =>
+              inputChangeHandler("duration", window.event.target.value)
+            } 
+            type="number" 
+            id="duration" />
+        </p>
+      </div>
+      <p className="actions">
+        <button onClick={resetHandler} type="reset" className="buttonAlt">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
 };
 
 export default UserInput;
