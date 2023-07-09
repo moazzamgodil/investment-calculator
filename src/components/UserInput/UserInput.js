40f5290483cca60a8822229296a4abba9b97faed
Anatolii Kravchenko <tol890@ukr.net>
2023-07-09 10:19:45 +0300

    [004] - Add managing state of user input fields

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
+const initialUserInput = {
+  'current-savings': 10000,
+  'yearly-contribution': 1200,
+  'expected-return': 7,
+  duration: 10
+};
+
 const UserInput = () => {
+  const [userInput, setUserInput] = useState(initialUserInput);
+
   const submitHandler = (event) => {
     event.preventDefault();
     // ...
@@ -6,12 +17,16 @@ const UserInput = () => {
   };
 
   const resetHandler = () => {
-    // ...
-    console.log("RESET");
+    setUserInput(initialUserInput);
   };
 
   const inputChangeHandler = (input, value) => {
-    console.log(input, value);
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input]: value,
+      };
+    });
   };
 
   return (
@@ -23,6 +38,7 @@ const UserInput = () => {
             onChange={() =>
               inputChangeHandler("current-savings", window.event.target.value)
             }
+            value={userInput['current-savings']}
             type="number"
             id="current-savings"
           />
@@ -33,6 +49,7 @@ const UserInput = () => {
             onChange={() =>
               inputChangeHandler("yearly-contribution", window.event.target.value)
             }
+            value={userInput['yearly-contribution']}
             type="number"
             id="yearly-contribution"
           />
@@ -47,6 +64,7 @@ const UserInput = () => {
             onChange={() =>
               inputChangeHandler("expected-return", window.event.target.value)
             }
+            value={userInput['expected-return']}
             type="number"
             id="expected-return"
           />
@@ -56,6 +74,7 @@ const UserInput = () => {
           <input onChange={() =>
               inputChangeHandler("duration", window.event.target.value)
             } 
+            value={userInput['duration']}
             type="number" 
             id="duration" />
         </p>
